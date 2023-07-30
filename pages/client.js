a731940a4a5e901c5504e3b0de425ebe14c3af92
Bruno_MH <brunomh141a@gmail.com>
2023-07-30 12:44:38 -0600

    fetch() request

---
+++ pages/client.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Auxiliar from './components/Auxiliar'
+
+export default function Client() {
+  return (
+    <div>
+        Hello from Client
+        <Auxiliar/>
+    </div>
+  )
+}
